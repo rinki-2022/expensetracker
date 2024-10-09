7ee4862f2d350a9fc9334022d1d7b045db64861f
rinki-2022 <rinkibansal2022@gmail.com>
2024-10-08 23:49:31 -0400

    Added hibernate validators

--- src/main/java/com/learnandgrow/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/com/learnandgrow/expensetrackerapi/controller/ExpenseController.java
@@ -2,6 +2,7 @@ package com.learnandgrow.expensetrackerapi.controller;
 
 import com.learnandgrow.expensetrackerapi.entity.Expense;
 import com.learnandgrow.expensetrackerapi.service.ExpenseService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -34,7 +35,7 @@ public class ExpenseController {
 
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping("/expenses")
-    public Expense saveExpenseDetails(@RequestBody Expense expense){
+    public Expense saveExpenseDetails(@Valid @RequestBody Expense expense){
         return expenseService.saveExpenseDetails(expense);
     }
 
--- src/main/java/com/learnandgrow/expensetrackerapi/entity/Expense.java
+++ src/main/java/com/learnandgrow/expensetrackerapi/entity/Expense.java
@@ -1,6 +1,9 @@
 package com.learnandgrow.expensetrackerapi.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,11 +29,20 @@ public class Expense {
     private Long id;
 
     @Column(name ="expense_name")
+    @NotBlank(message = "Expense name must not be null")
+    @Size(min = 3, message = "Expense name must be atleast 3 characters")
     private String name;
+
     private String description;
+
+    @NotBlank(message = "Category must not be null")
     private String category;
+
     @Column(name ="expense_amount")
+    @NotNull(message = "Expense amount must not be null")
     private BigDecimal amount;
+
+    @NotNull(message = "Date must not be null")
     private Date date;
 
     @Column(name = "created_at", nullable = false, updatable = false)
