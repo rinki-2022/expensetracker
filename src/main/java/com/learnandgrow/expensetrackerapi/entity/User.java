aacf529269e8680d8e8701b06d62233888ea6c65
rinki-2022 <rinkibansal2022@gmail.com>
2024-10-13 21:55:07 -0400

    added user entity

---
+++ src/main/java/com/learnandgrow/expensetrackerapi/entity/User.java
@@ -0,0 +1,32 @@
+package com.learnandgrow.expensetrackerapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tbl_users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private Long age;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @CreationTimestamp
+    private Timestamp updatedAt;
+}
